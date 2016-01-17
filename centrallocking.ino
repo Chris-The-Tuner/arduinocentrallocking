ed80db9afd000daa43128b9bea74e57a238f888b
Chris-The-Tuner <Chris-The-Tuner@users.noreply.github.com>
2016-01-17 22:53:11 +0100

    Main Program

--- centrallocking.ino
+++ centrallocking.ino
@@ -8,7 +8,7 @@
 //Settings
 int lightpin = 5; //pin for light (warning lights)
 int hornpin = 6; //pin for the horn
-int hornlength = 250; //time for horn to be on (500 = 1/2 second)
+int hornlength = 50; //time for horn to be on (500 = 1/2 second)
 int motordelay = 500; //can be adjusted to match your motors (time needed to upen/close the doors)
 
 //do only touch if your arduino does not match the arduino nano !!!
@@ -33,35 +33,39 @@ void setup() //setting up the pins
 void loop() //loop and wait for a signal
 {
  val = digitalRead(2); //read if RF signal is comming in
-
- if (val == LOW) //react if pin is LOW (RF signal incomming)
+ if (val == HIGH) //react if pin is LOW (RF signal incomming)
  {
   if (locked == 0) //if doors are locked react like this
   {
    digitalWrite(relais1pin, LOW); //polarity is now set to "closing" (mainly the relay should now be off to save battery power)
    digitalWrite(lightpin, HIGH); //from now on lights are blinking (visual making of closing sequence)
    digitalWrite(relais2pin, HIGH); //door motors should close now
-   delay(motordelay);
+   delay(motordelay); //waiting the motor delay
    digitalWrite(relais2pin, LOW); //doors should be closed by now (if not adjust "closingdelay" !)
    digitalWrite(hornpin, HIGH); //horn goes off for the set time
-   delay(hornlength);
+   delay(hornlength); //waiting until horn should turn off
    digitalWrite(hornpin, LOW); //horn goes off
+   delay(500); //waiting one more moment for the lights to turn off
    digitalWrite(lightpin, LOW); //lights should be turned off now
    locked = 1; //setting value to locked doors
    delay(5000); //sleeping for 5 seconds (don't un-lock the doors again by accident)
   }
-
-
+ }
+ 
+ val = digitalRead(2); //read if RF signal is comming in
+ if (val == HIGH) //react if pin is LOW (RF signal incomming)
+ {
   if (locked == 1) //if doors are unlocked react like this
   {
    digitalWrite(relais1pin, HIGH); //polarity is now set to "opening"
    digitalWrite(lightpin, HIGH); //from now on lights are blinking (visual making of closing sequence)
    digitalWrite(relais2pin, HIGH); //door motors should open now
-   delay(motordelay);
+   delay(motordelay); //waiting the motor delay
    digitalWrite(relais2pin, LOW); //doors should be open by now (if not adjust "motordelay" !)
    digitalWrite(hornpin, HIGH); //horn turns on for the set time
-   delay(hornlength);
+   delay(hornlength); //waiting until the horn should turn off
    digitalWrite(hornpin, LOW); //horn turns off
+   delay(500); //waiting one more moment for the lights to turn off
    digitalWrite(lightpin, LOW); //lights should be turned off now
    digitalWrite(relais1pin, LOW); //turning off the relay (state: closing) to save battery power)
    locked = 0; //setting value to un-locked doors
